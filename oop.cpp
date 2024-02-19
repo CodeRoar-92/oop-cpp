4789e914367bd1b5fce884a042125f17a1a54fc5
CodeRoar-92 <coderoar92@gmail.com>
2024-02-19 10:36:25 +0500

    remove bug

--- oop.cpp
+++ oop.cpp
@@ -1,41 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class person{
-    private:
-    int age;
-    string name;
-    string meaning_of_name;
-    int number;
-    public:
-    void input(){
-        cout<<"Enter Your age: "<<endl;
-        cin>>age;
-        cout<<"Enter Your name: "<<endl;
-        cin>>name;
-        cout<<"Enter Your meaning_of_name: "<<endl;
-        cin>>meaning_of_name;
-        cout<<"Enter Your number: "<<endl;
-        cin>>number;
-    }
-    void show(){
-        cout<<"Age: " <<age<<endl;
-        cout<<"Name: " <<name<<endl;
-        cout<<"meaning_of_name: " <<meaning_of_name<<endl;
-        cout<<"Number: " <<number<<endl;
-    }
+class person
+{
+private:
+  int age;
+  string name;
+  string meaning_of_name;
+  int number;
+
+public:
+  void input()
+  {
+    cout << "Enter Your age: " << endl;
+    cin >> age;
+    cout << "Enter Your name: " << endl;
+    cin >> name;
+    cout << "Enter Your meaning_of_name: " << endl;
+    cin >> meaning_of_name;
+    cout << "Enter Your number: " << endl;
+    cin >> number;
+  }
+  void show()
+  {
+    cout << "Age: " << age << endl;
+    cout << "Name: " << name << endl;
+    cout << "meaning_of_name: " << meaning_of_name << endl;
+    cout << "Number: " << number << endl;
+  }
 };
-int main(){
-    person abdullah,rajab,afaq,rida,ali, ahmad;
-    abdullah.input();
-    abdullah.show();
-    rajab.input();
-    rajab.show();
-      afaq.input();
-    afaq.show();
-      rida.input();
-    rida.show();
-      ali.input();
-    ali.show();
-    ahmad.input();
-    ahmad.show();
+int main()
+{
+  person abdullah, rajab, afaq,ali, ahmad;
+  abdullah.input();
+  abdullah.show();
+  rajab.input();
+  rajab.show();
+  afaq.input();
+  afaq.show();
+  ali.input();
+  ali.show();
+  ahmad.input();
+  ahmad.show();
 }
