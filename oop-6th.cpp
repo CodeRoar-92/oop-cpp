1d15ff814513af3299b616149b5cd69bf8d8ab1b
CodeRoar-92 <coderoar92@gmail.com>
2024-02-21 10:09:47 +0500

    Constructor and its properties

---
+++ oop-6th.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class myClass{
+public:
+myClass(){// is a member fuction which has same name a class name;
+    cout<<"Abdullah Shehzada:\n";
+}
+};
+int main(){
+    myClass obj,obj1,obj2,obj3;//Constructer is automatically called we don't need to call
+}
+//obj,obj... are objects
+
